748f28981d9f61082792e0f44a48fc7d34904294
3rooks <brookslynx@gmail.com>
2022-04-28 20:14:35 -0300

    Hook useState (InitialState, SetState, Rerenders)

--- src/components/UserRow.jsx
+++ src/components/UserRow.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import UserRole from './UserRole';
 import style from './UserRow.module.css';
 import UserStatus from './UserStatus';
@@ -5,16 +6,27 @@ import UserStatus from './UserStatus';
 const UserRow = ({ data }) => {
 	const { name, active, role } = data;
 
+	const [isActive, setIsActive] = useState(active);
+
 	return (
 		<div className={style.user}>
 			<div className={style.name}>
 				<span>{name}</span>
 			</div>
 			<div className={style.status}>
-				<UserStatus active={active} />
+				<UserStatus active={isActive} />
 			</div>
 			<div className={style.role}>
 				<UserRole role={role} />
+			</div>
+			<div className={style.action}>
+				<button
+					onClick={() => {
+						setIsActive(!isActive);
+					}}
+				>
+					{isActive ? 'Desactivar' : 'Activar'}
+				</button>
 			</div>
 		</div>
 	);
